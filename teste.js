15e177e8bea188fb1b06b23af03fea12395d5eb5
Marcelo Galvao <mgalvao2012@gmail.com>
2022-05-22 16:55:42 -0300

    criada da função usuarioDefiniuUnidade

--- teste.js
+++ teste.js
@@ -1,5 +1,13 @@
 const util = require('./util');
 
+async function calculate(x) {
+    await new Promise((resolve, reject) => {
+        x++
+        console.log(x)
+        resolve(x)
+    });
+}
+
 async function teste(x) {
     try {
         await new Promise((resolve, reject) => {
@@ -20,7 +28,8 @@ async function teste(x) {
 
 var x = 0;
 teste(x)
-    .then((retorno) => { teste(retorno) })
+    .then((y) => { console.log('y '+y); })
+    .then((retorno) => { teste(x); })
     .then(() => {
         let data_atual = util.formatDate(new Date(), 2)
         console.log(data_atual)
--- util.js
+++ util.js
@@ -36,12 +36,11 @@ module.exports.enviarEmailHTML = function (email, subject, content) {
     });
 }
 
-
 function padTo2Digits(num) {
   return num.toString().padStart(2, "0");
 }
 
-module.exports.formatDate = function (date, format) {
+module.exports.formatDate = function(date, format) {
   if (format == 1) {
     return (
       [
@@ -71,4 +70,49 @@ module.exports.formatDate = function (date, format) {
       ].join(":")
     );
   }
+}
+
+module.exports.usuarioDefiniuUnidade = async function(request, response) {
+  await new Promise((resolve, _reject) => {
+    // verifica se o usuario já definiu sua unidade para liberar o acesso a outras funcionalidades
+    if (request.session.unidade_usuario == undefined) {
+      user_id = request.oidc.user.sub;
+      pool.query(
+        `SELECT codigo FROM vagas WHERE disponivel = true ORDER BY codigo;
+        SELECT * FROM unidades WHERE user_id = '${user_id}';
+        SELECT unidade FROM unidades WHERE user_id IS NULL ORDER BY unidade;`,
+        (error, results) => {
+          if (error) {
+            console.log(error.message);
+          } else {
+            if (
+              results[1].rows[0] == undefined ||
+              results[1].rows[0].unidade == null
+            ) {
+              response.render("meusdados.ejs", {
+                user_id: user_id,
+                email: request.oidc.user.email,
+                name: request.oidc.user.name,
+                unidade: null,
+                vagas_escolhidas: null,
+                mensagem: [
+                  "warning",
+                  "Atenção!",
+                  "Você precisa definir a sua unidade para ter acesso a outras funcionalidades!",
+                ],
+                lista_vagas: results[0].rows,
+                lista_unidades: results[2].rows,
+                vaga_sorteada: null,
+                usuario_admin: request.session.usuario_admin,
+              });
+              resolve('meusdados');
+            } else {
+              request.session.unidade_usuario = results[1].rows[0].unidade;
+            }
+          }
+        }
+      );
+    }
+    resolve('OK');
+  });  
 }
